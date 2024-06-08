827d7cae574540ba848a84c6d863342a14fc2df7
“Anish <anishgupta@urbancompany.com>
2024-06-08 12:53:01 +0530

    Initial Commit

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Anish Gupta on 08/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
